c12cde0df6832375f3d7cbc0fb6e85a8cd5d1627
keerthi <k.rapolu1@nhs.net>
2022-02-03 00:26:21 +0000

    Initial commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,18 @@
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        ArrayList<String> ar=new ArrayList<>(Arrays.asList("wew","weeeeeee"));
+
+        System.out.println( ar );
+    }
+}
